0397afc4ea39c11a4aedcd7e3b59e7f97f605b93
sunnydj69 <85759813+sunnydj69@users.noreply.github.com>
2022-01-09 10:03:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://Sunnykobunni:670076sunnY$@cluster0.wkmxu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5051423279:AAG5KEZeioTIub_qcIBogbaY8wwKhsgRkpM',//bot token
+
+bot_name:'TTBiggBossV2Bot',//bot username without @
+
+bot_admin:1342340343,// admin telegram id
+
+channelsList : ['@airdropspanther','@airdropadvocates','@legitairdrops010','@airdropbuilders','@airdropborder','@airdropruler'],// channels list
+
+channelscheck : ['@airdropspanther','@airdropadvocates','@airdropbuilders','@payoutsplanet','@airdropborder','@airdropruler'],
+
+reffer_bonus:0.2,// refferal bonus amount
+
+min_wd:0.3,//min withdrawal amount
+
+daily_bonus:0,// daily bonus amount
+
+currency:'Thunder Token', //bot cuurency
+
+cb_api_key:'zQaZ1xbCfC8iNxYk',//coinbase api key
+
+cb_api_secret:'33JqlMXnVxpowHLDW9aZjqUnIthUxaAL',// coinbase secret key
+
+cb_account_id:'3b451efc-669e-5f84-b0d9-b545e2dc3424',// counbase account id for the bot currency
+
+payment_channel:'@PayoutsPlanet'// payment channel username
+
+}
